c4fa6f44dcf5541fe5e058d1306a03494df80b3e
Joanne Lee <vernapii@gmail.com>
2018-05-02 22:34:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,7 +12,8 @@ class App < Sinatra::Base
   end
 
   post '/checkout' do
-    @session = :session_secret << params
+    @session = {}
+    @session[:item] = params[:item]
 
     erb :checkout
   end
